28d4cebfa35cef0e49d883db77a99b78255584d1
Cele-stino <celestino.resteghini@gmail.com>
2025-04-17 18:08:44 +0200

    Aggiornamento classi Ristorante e UtenteNonRegistrato

--- src/UtenteNonRegistrato.java
+++ src/UtenteNonRegistrato.java
@@ -1,8 +1,10 @@
+import java.util.Date;
+
 public class UtenteNonRegistrato extends Utente {
     public UtenteNonRegistrato() {}
 
 
-    public void registrazione() {
+    public void registrazione(String username, String nome, String cognome, Date dataNascita, String password, boolean ristoratore, Luogo luogo) {
         //implementa metodo
     }
 

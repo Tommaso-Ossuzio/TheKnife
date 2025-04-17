ff07bfe415dcdce6aacc1026f0fb7030d70f822a
sonoFrangu <franguelli.matteo05@gmail.com>
2025-04-17 17:22:53 +0200

    Fix classe GestioneFile -> metodo leggiFile

--- src/GestioneFile.java
+++ src/GestioneFile.java
@@ -8,20 +8,25 @@ public class GestioneFile {
     private String percorsoFile;
 
     public GestioneFile() {
+        String nomeCartella = "doc";
         String nomeFile = "michelin_my_maps.csv";
-        this.percorsoFile = System.getProperty("user.dir") + File.separator + nomeFile;
+        this.percorsoFile = System.getProperty("user.dir") + File.separator + nomeCartella + File.separator + nomeFile;
     }
 
     public void leggiFile() {
         try (BufferedReader br = new BufferedReader(new FileReader(percorsoFile))) {
             String riga;
+            int numeroRiga = 1;
 
             while ((riga = br.readLine()) != null) {
                 String[] valori = riga.split(",");
+
+                System.out.print(numeroRiga + ": ");
                 for (String valore : valori) {
                     System.out.print(valore.trim() + " | ");
                 }
                 System.out.println();
+                numeroRiga++;
             }
 
         } catch (IOException e) {
